74911145e30a5a15fde013fb56671f6b0fd1da22
DaniilPak <101089638+DaniilPak@users.noreply.github.com>
2023-06-23 00:16:05 +0600

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -44,6 +44,6 @@ const swaggerDocument = require("../swagger.json");
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 /// Main entry
-app.listen(3000, () => {
+app.listen(80, () => {
   console.log(`Server Started at ${3000}`);
 });
