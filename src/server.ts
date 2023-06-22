40a3ebfc1531fd52c4263f1a91ad3c9bbfd9a17d
DaniilPak <1asumeasume1@gmail.com>
2023-06-22 23:46:35 +0600

    #wip

--- src/server.ts
+++ src/server.ts
@@ -20,21 +20,18 @@ app.use(cors());
 app.use(express.json());
 
 /// API entry
-import { DataRoute } from "./routes/DataRoute";
 import { UserRoute } from "./routes/UserRoute";
 import { ReceiptRoute } from "./routes/ReceiptRoute";
 import { LoyaltyCardRoute } from "./routes/LoyaltyCardRoute";
 import { BusinessRoute } from "./routes/BusinessRoute";
 import { AuthRoute } from "./routes/AuthRoute";
 
-const dataRoute = new DataRoute();
 const userRoute = new UserRoute();
 const receiptRoute = new ReceiptRoute();
 const loyaltyCardRoute = new LoyaltyCardRoute();
 const businessRoute = new BusinessRoute();
 const authRoute = new AuthRoute();
 
-app.use("/api", dataRoute.getRouter());
 app.use("/api/user", userRoute.getRouter());
 app.use("/api/receipt", receiptRoute.getRouter());
 app.use("/api/loyaltycard", loyaltyCardRoute.getRouter());
